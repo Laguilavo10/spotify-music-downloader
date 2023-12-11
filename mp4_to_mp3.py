98fc687824f95201f236197ffb33b3a2a01b6ac4
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-12-11 14:22:44 -0500

    Update mp4_to_mp3.py to set bitrate and fps for audio conversion

--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -4,6 +4,7 @@ from utils.config import pathSaveVideos, pathSaveMusic
 def mp4_to_mp3(name):
     video = VideoFileClip(pathSaveVideos+name+'.mp4')
     audio = video.audio
-    audio.write_audiofile(pathSaveMusic+name+'.mp3', logger=None)
+    audio.write_audiofile(pathSaveMusic+name+'.mp3', logger=None, bitrate='128k', fps=44100)
     audio.close()
     video.close()
+
