6f60ecee918e4f32908bfb6b802e32dd0614efa3
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-12-11 14:56:26 -0500

    Update spotipy_redirect_uri in get_tracks.py

--- get_tracks.py
+++ get_tracks.py
@@ -1,11 +1,11 @@
 import spotipy
 import spotipy.util as util
 import sys
-from utils.config import spotipy_client_id, spotipy_client_secret, tracks_count
+from utils.config import spotipy_client_id, spotipy_client_secret, tracks_count, spotipy_redirect_uri
 
 def get_tracks_data():
     scope = 'user-library-read'
-    token = util.prompt_for_user_token(sys.argv[0],scope,client_id=spotipy_client_id,client_secret=spotipy_client_secret,redirect_uri='https://google.com')
+    token = util.prompt_for_user_token(sys.argv[0],scope,client_id=spotipy_client_id,client_secret=spotipy_client_secret,redirect_uri=spotipy_redirect_uri)
     sp = spotipy.Spotify(auth=token)
     results = sp.current_user_saved_tracks(limit=tracks_count)
     return results
--- utils/config.py
+++ utils/config.py
@@ -4,6 +4,7 @@ load_dotenv('.env')
 
 spotipy_client_id = os.getenv("SPOTIPY_CLIENT_ID")
 spotipy_client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+spotipy_redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
 pathSaveMusic = os.getenv("PATH_SAVE_MUSIC")
 pathSaveVideos = os.getenv("PATH_SAVE_VIDEO_MUSIC")
 tracks_count= os.getenv("TRACKS_COUNT")
