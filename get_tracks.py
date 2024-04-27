101574b5fb1110703c4d10d6c5b66b361a3b845d
Andres Laguilavo <lagui2003@gmail.com>
2024-04-27 18:40:04 -0500

    Update get_tracks.py to limit the number of saved tracks to 1

--- get_tracks.py
+++ get_tracks.py
@@ -7,5 +7,5 @@ def get_tracks_data():
     scope = 'user-library-read'
     token = util.prompt_for_user_token(sys.argv[0],scope,client_id=spotipy_client_id,client_secret=spotipy_client_secret,redirect_uri=spotipy_redirect_uri)
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks(limit=tracks_count)
+    results = sp.current_user_saved_tracks(limit=1)
     return results
