9a7f697598e677125ba73c26a6b80b8edbdfd7a6
Jonatandb <jonatandb@gmail.com>
2024-05-12 12:59:09 -0300

    Add code to fix error when specified music folder path doesn't exists

--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -4,6 +4,8 @@ from utils.config import pathSaveVideos, pathSaveMusic
 def mp4_to_mp3(name):
     video = VideoFileClip(pathSaveVideos+name+'.mp4')
     audio = video.audio
+    if not os.path.exists(pathSaveMusic):
+        os.mkdir(pathSaveMusic)
     audio.write_audiofile(pathSaveMusic+name+'.mp3', logger=None, bitrate='128k', fps=44100)
     audio.close()
     video.close()
