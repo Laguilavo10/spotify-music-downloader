4133eb9298484b59a9c800f3ebdd41311af39777
Jonatandb <jonatandb@gmail.com>
2024-05-12 13:02:31 -0300

    Improve/simplify "PATH_SAVE_VIDEO_MUSIC" env variable name

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from dotenv import load_dotenv
 load_dotenv('.env')
 
@@ -6,5 +6,5 @@ spotipy_client_id = os.getenv("SPOTIPY_CLIENT_ID")
 spotipy_client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 spotipy_redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
 pathSaveMusic = os.getenv("PATH_SAVE_MUSIC")
-pathSaveVideos = os.getenv("PATH_SAVE_VIDEO_MUSIC")
+pathSaveVideos = os.getenv("PATH_SAVE_VIDEO")
 tracks_count= os.getenv("TRACKS_COUNT")
