1f1cb19130c5310d3d3eb2a0f12cf465e4aa94c0
Andres Laguilavo <lagui2003@gmail.com>
2024-07-15 12:59:33 -0500

    Refactor download_song.py to improve code readability and add console output for the YouTube video link

--- download_song.py
+++ download_song.py
@@ -5,6 +5,8 @@ from utils.sanitize_filename import sanitize_filename
 def download_tracks(song):
     searchedSong = Search(f'{song} letra')
     video_id = searchedSong.results[0].video_id
-    yt = YouTube(f'http://youtube.com/watch?v={video_id}')
+    url_video = f'http://youtube.com/watch?v={video_id}'
+    print("\t\t" + "Link: ", url_video)
+    yt = YouTube(url_video)
     yt.streams.get_lowest_resolution().download(output_path=pathSaveVideos)
     return sanitize_filename(yt.title)
